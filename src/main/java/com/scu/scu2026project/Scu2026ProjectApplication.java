94c30a260604ba793aec6d5d5f8ccc5909b1fb6a
Dejoy <Dejoy1202@gmail.com>
2026-07-20 15:56:43 +0800

    首次提交：创建SpringBoot项目

---
+++ src/main/java/com/scu/scu2026project/Scu2026ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.scu.scu2026project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Scu2026ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Scu2026ProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scu/scu2026project/Scu2026ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scu.scu2026project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Scu2026ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
