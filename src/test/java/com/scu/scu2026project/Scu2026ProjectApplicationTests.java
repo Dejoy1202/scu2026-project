007dd69ca351eeb962c8161edd023040f7eed765
Dejoy <Dejoy1202@gmail.com>
2026-07-21 13:59:15 +0800

    查询所有用户列表的单元测试

--- src/test/java/com/scu/scu2026project/Scu2026ProjectApplicationTests.java
+++ src/test/java/com/scu/scu2026project/Scu2026ProjectApplicationTests.java
@@ -1,13 +1,26 @@
 package com.scu.scu2026project;
 
+import com.scu.scu2026project.entity.User;
+import com.scu.scu2026project.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Scu2026ProjectApplicationTests {
+import java.util.List;
 
+@SpringBootTest
+@Slf4j
+class Scu2026ProjectApplicationTests {
+    //注入持久层接口
+    @Autowired
+    private UserMapper userMapper;
     @Test
-    void contextLoads() {
+    void getUserList(){
+        //查询所有用户信息
+        List<User> userList = userMapper.selectList(null);
+        log.info("查询的用户列表：{}", userList);
+        log.info("用户个数：{}", userList.size());
     }
 
 }
