dd60c9666feed3abb805ae23558837092ae4431b
Dejoy <Dejoy1202@gmail.com>
2026-07-21 20:15:25 +0800

    新增多种单元测试

--- src/test/java/com/scu/scu2026project/Scu2026ProjectApplicationTests.java
+++ src/test/java/com/scu/scu2026project/Scu2026ProjectApplicationTests.java
@@ -1,5 +1,6 @@
 package com.scu.scu2026project;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.scu.scu2026project.entity.User;
 import com.scu.scu2026project.mapper.UserMapper;
 import lombok.extern.slf4j.Slf4j;
@@ -22,5 +23,64 @@ class Scu2026ProjectApplicationTests {
         log.info("查询的用户列表：{}", userList);
         log.info("用户个数：{}", userList.size());
     }
+
+    //根据用户名进行查询
+    @Test
+    void getUser() {
+        //创建查询条件
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        //1.列名 2.值
+        queryWrapper.like("USER_LOGNAME", "d")
+                .eq("USER_ROLE", 3);
+        List<User> userList = userMapper.selectList(queryWrapper);
+        log.info("查询的用户列表：{}", userList);
+    }
+/*
+    @Test
+    void inertUser() {
+        User user = new User();
+        user.setLoginName("laowang");
+        user.setPassword("123456");
+        user.setRealName("老王");
+        user.setEmail("laowangqq.com");
+        user.setRoleId(2);
+        user.setState(1);
+        int result = userMapper.insert(user);
+        log.info("新增用户的结果：{}", result);
+    }*/
+
+    /*
+    @Test
+    void updateUser() {
+        User user = new User();
+        user.setId(7);
+        user.setLoginName("tony");
+        user.setEmail("tony@qq.com");
+        int result = userMapper.updateById(user);
+        log.info("修改用户的结果：{}", result);
+    }*/
+
+    @Test
+    void getUserById() {
+        User user = userMapper.selectById(7);
+        log.info("查询的用户信息：{}", user);
+    }
+
+    /*
+    @Test
+    void deleteUserById() {
+        int result = userMapper.deleteById(7);
+        log.info("删除用户的结果：{}", result);
+    }*/
+
+    @Test
+    void deleteUser() {
+        User user = new User();
+        user.setId(1);
+        user.setState(0);
+        int result = userMapper.updateById(user);
+        log.info("逻辑删除用户的结果：{}", result);
+    }
+
 
 }
