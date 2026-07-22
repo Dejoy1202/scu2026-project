84b54cee2c484789316efc2b528e129b8a67aaa3
Dejoy <Dejoy1202@gmail.com>
2026-07-22 11:32:28 +0800

    开发登录DTO

---
+++ src/main/java/com/scu/scu2026project/dto/loginDTO.java
@@ -0,0 +1,11 @@
+package com.scu.scu2026project.dto;
+
+import lombok.Data;
+
+@Data
+public class loginDTO {
+    // todo
+    private String username;
+    // todo
+    private String password;
+}
